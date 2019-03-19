1b905b4ee7ec64f227031ce89a8621cc41ecb52c
Marcel Benders <marcel.benders@cleverbridge.com>
2019-03-19 17:07:19 +0100

    (App-86) Added types for ActiveMeasurePoin

---
+++ InputTypes/ActiveMeasurePointInputType.cs
@@ -0,0 +1,17 @@
+﻿using com.b_velop.stack.Classes.Models;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace com.b_velop.stack.GraphQl.InputTypes
+{
+    public class ActiveMeasurePointInputType : InputObjectGraphType<ActiveMeasurePoints>
+    {
+        public ActiveMeasurePointInputType()
+        {
+
+        }
+    }
+}
---
+++ Types/ActiveMeasurePointType.cs
@@ -0,0 +1,19 @@
+﻿using com.b_velop.stack.Classes.Models;
+using com.b_velop.stack.GraphQl.Contexts;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace com.b_velop.stack.GraphQl.Types
+{
+    public class ActiveMeasurePointType : ObjectGraphType<ActiveMeasurePoints>
+    {
+        public ActiveMeasurePointType(
+            MeasureContext measureContext)
+        {
+
+        }
+    }
+}
